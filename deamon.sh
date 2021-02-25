a9dd961b5160a6cfdb2b0d49642b8aa4bd97d287
Carlos Faustino <70132833+taidos@users.noreply.github.com>
2021-02-25 18:53:25 +0000

    Update deamon.sh

--- deamon.sh
+++ deamon.sh
@@ -3,4 +3,4 @@ wget http://developer.axis.com/download/distribution/apps-sys-utils-start-stop-d
 tar zxvf apps-sys-utils-start-stop-daemon-IR1_9_18-2.tar.gz
 cd apps/sys-utils/start-stop-daemon-IR1_9_18-2
 gcc start-stop-daemon.c -o start-stop-daemon
-cp start-stop-daemon /usr/sbin/
+cp start-stop-daemon /sbin/
